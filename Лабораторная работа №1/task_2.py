1872e3fd30993beb9b30927856425716168d52e0
Vaasiilii <vasiliiandrv@gmail.com>
2024-11-14 20:35:53 +0300

    Update task_2.py

--- Лабораторная работа №1/task_2.py
+++ Лабораторная работа №1/task_2.py
@@ -5,7 +5,7 @@ stroki = 50
 chislo_simvolov = 24
 for_one_simvol = 4  # 4 байта для хранения одного символа
 
-volume_of_disketa_in_b = 1.44*1024*1024  # перевод объема дискеты в байты
+volume_of_disketa_in_b = volume*1024*1024  # перевод объема дискеты в байты
 volume_of_one_book = for_one_simvol*chislo_simvolov*stroki*pages
 count_of_books = volume_of_disketa_in_b//volume_of_one_book
 print(f"Количество книг, помещающихся на дискету: {count_of_books:.0f}")
