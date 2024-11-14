098a676f2883d279200710b201e56894aae209a8
Vaasiilii <vasiliiandrv@gmail.com>
2024-11-14 20:35:27 +0300

    Update task_1.py

--- Лабораторная работа №1/task_1.py
+++ Лабораторная работа №1/task_1.py
@@ -1,8 +1,5 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-
-# TODO заменить значение пропущенного элемента средним арифметическим
-delete_number = numbers[4]
-new_numbers = [2, -93, -2, 8, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-average_instaed_of_missing = sum(new_numbers)/(len(new_numbers)+1)
+numbers[4] = 0
+average_instaed_of_missing = sum(numbers)/len(numbers)
 numbers[4] = average_instaed_of_missing
 print("Измененный список:", numbers)
