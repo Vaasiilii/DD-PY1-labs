6b70519d267bcddaf0f5774b111c03b94c69de47
Vaasiilii <vasiliiandrv@gmail.com>
2024-11-28 13:12:20 +0300

    Update Задание №1_Лаб №2.py
    Исправил неточность в цикле - лишние условие

--- Задание №1_Лаб №2.py
+++ Задание №1_Лаб №2.py
@@ -3,7 +3,7 @@ salary = 5000          # Ежемесячная зарплата
 spend = 6000           # Траты за первый месяц
 increase = 0.05        # Ежемесячный рост цен
 count = 0              # Счетчик количества месяцев, которое можно протянуть без долгов
-while spend - salary < money_capital:
+while True:
     count += 1
     money_capital -= spend - salary
     spend *= 1 + increase
